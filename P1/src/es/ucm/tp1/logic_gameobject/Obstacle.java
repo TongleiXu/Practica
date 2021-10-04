2f50684fab40ecd4a8b9cae1653df0845365a1b4
TongleiXu <79459437+TongleiXu@users.noreply.github.com>
2021-10-05 00:59:27 +0200

    test

--- P1/src/es/ucm/tp1/logic_gameobject/Obstacle.java
+++ P1/src/es/ucm/tp1/logic_gameobject/Obstacle.java
@@ -1,12 +1,12 @@
 	package es.ucm.tp1.logic_gameobject;
 
 public class Obstacle {
-/*	Comportamiento: No se mueve. El coche muere cuando choca contra él.
-	Resistencia: 1 punto de resistencia (en futuras extensiones tendremos muros más
-	fuertes y el usuario podrá dispararles o tirarles bombas).
+/*	Comportamiento: No se mueve. El coche muere cuando choca contra 茅l.
+	Resistencia: 1 punto de resistencia (en futuras extensiones tendremos muros m谩s
+	fuertes y el usuario podr谩 dispararles o tirarles bombas).
 */
 	private static int Resistencia = 1;
 	private static int x;
 	private static int y;
-	private final static String SYMBOL = " ";
+	private final static String SYMBOL = "░";
 }
